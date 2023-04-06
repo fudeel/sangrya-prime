e8ef0f51b68951ef1bd21b44af66a4e756307d3c
Fudeel <fadil_mucia@hotmail.com>
2023-04-06 16:46:25 +0200

    authentication with auth0 and redirecting to private

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "@auth0/auth0-angular";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'private',
+    loadChildren: () => import('./private/private.module').then(m => m.PrivateModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'private',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    AuthModule.forRoot({
+      domain: 'sangrya-development.eu.auth0.com',
+      clientId: 'uVaDkPLi8kG9TzwDsguceJQx1wBO0VKn',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
