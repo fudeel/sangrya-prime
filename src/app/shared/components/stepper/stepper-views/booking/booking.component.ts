41def7a38b5296bb32da9008477d569d998809f9
fudeel <17783461+fudeel@users.noreply.github.com>
2023-04-15 16:32:03 +0200

    dates selection

--- src/app/shared/components/stepper/stepper-views/booking/booking.component.ts
+++ src/app/shared/components/stepper/stepper-views/booking/booking.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {StepperService} from "../../stepper.service";
 import {Router} from "@angular/router";
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-booking',
@@ -12,18 +14,33 @@ export class BookingComponent implements OnInit, OnDestroy {
 
   rangeDates: Date[] = [];
 
-  bookingForm = this.fb.group({
-    dates: ['', Validators.required],
-  });
+  bookingForm: FormGroup | undefined;
+
+  datesSubscription: Subscription | undefined;
 
   minDate: Date = new Date();
   today = new Date();
   maxDate: Date = new Date(this.today.setDate(this.today.getDate() + 45));
 
-  constructor(private readonly stepperService: StepperService, private readonly router: Router, private fb: FormBuilder) {
+  times = ['once', 'twice', 'three-times', 'four-times'];
+  hoursPerTime = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+
+  constructor(private readonly stepperService: StepperService, private readonly router: Router, private fb: FormBuilder, private readonly location: Location) {
   }
 
   ngOnInit(): void {
+    this.bookingForm = new FormGroup({
+      dates: new FormControl<Date | null>(null)
+    });
+  }
+
+  onBack() {
+    this.location.back();
+  }
+
+  onNext() {
+    this.router.navigate([`private/engage/payment`], { queryParamsHandling: 'merge' });
   }
 
   ngOnDestroy(): void {
