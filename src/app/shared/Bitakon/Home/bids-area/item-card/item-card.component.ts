26774d4095bcce4c0a2eb7296bb487659ead63f4
fadilmuca <fadil.muca@cognizant.com>
2023-05-24 20:12:27 +0200

    item card

--- src/app/shared/Bitakon/Home/bids-area/item-card/item-card.component.ts
+++ src/app/shared/Bitakon/Home/bids-area/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {CartService} from "../../../../services/cart.service";
 
 @Component({
@@ -6,12 +6,12 @@ import {CartService} from "../../../../services/cart.service";
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss']
 })
-export class ItemCardComponent {
+export class ItemCardComponent implements OnInit, OnChanges{
 
   @Input() item: any;
 
-  @Output() addToCart: EventEmitter<any> = new EventEmitter<any>();
   @Output() selectItem: EventEmitter<any> = new EventEmitter<string>();
+  @Output() addToCart: EventEmitter<any> = new EventEmitter<any>();
 
   isInCart: boolean = false;
 
@@ -21,10 +21,16 @@ export class ItemCardComponent {
     })
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+  }
 
+
+  ngOnInit(): void {
+    this.isInCart = this.cartService.isItemInCart(this.item._id);
+  }
   addRemoveFromCartOrShowDetails(item: any, button: 'addRemove' | 'details') {
-    console.log('item-card.component.ts: addRemoveFromCartOrShowDetails(): item: ', item, ' button: ', button);
     this.selectItem.emit({item, button: button});
+    this.isInCart = this.cartService.isItemInCart(item._id);
   }
 
 }
