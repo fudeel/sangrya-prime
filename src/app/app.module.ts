1d87050568f7528e05c1355cf4ded34765586d35
fadilmuca <fadil.muca@cognizant.com>
2023-05-25 12:05:01 +0200

    migrate to pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import {SharedModule} from "./shared/shared.module";
 import {TableModule} from "primeng/table";
 import {DataViewModule} from "primeng/dataview";
 import {RatingModule} from "primeng/rating";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,13 @@ import {RatingModule} from "primeng/rating";
     SharedModule,
     TableModule,
     DataViewModule,
-    RatingModule
+    RatingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
